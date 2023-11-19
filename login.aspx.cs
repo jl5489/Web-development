bfe93f30040f4690034d69a996c1023c46685129
yunlungp <148225270+yunlungp@users.noreply.github.com>
2023-11-19 22:18:52 +0800

    Add files via upload

---
+++ login.aspx.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Assignment
+{
+    public partial class login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnRegister_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("register.aspx");
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            try
+             {
+                SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["TeamsDatabaseConnString"].ConnectionString);
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("SELECT * FROM [User] WHERE Username=@Uname AND Password = @pwd", conn);
+                cmd.Parameters.AddWithValue("@Uname", txtUname.Text);
+                cmd.Parameters.AddWithValue("@pwd", txtPwd.Text);
+                SqlDataReader rdr = cmd.ExecuteReader();
+
+                if (rdr.HasRows)
+                {
+                    while (rdr.Read())
+                    {
+                        //Session["UserName"] = rdr["username"].ToString();
+                        int userRole = Convert.ToInt16(rdr["isAdmin"].ToString());
+
+                        switch (userRole)
+                        {
+                            case 0:
+                                //Response.Redirect("~/SimpleGridView.aspx");
+                                lblMessage.Text = "user";
+                                break;
+                            case 1:
+                                //Response.Redirect("~/EditCategory.aspx");
+                                lblMessage.Text = "admin";
+                                break;
+
+                        }
+                    }
+                }
+                else
+                {
+                    lblMessage.Text = "Username and / or password not found.";
+                }
+            conn.Close();
+            }
+            catch (Exception ex)
+            {
+                Response.Write(ex.ToString());
+            }
+        }
+    }
+}
---
+++ register.aspx.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Assignment
+{
+    public partial class register : System.Web.UI.Page
+    {
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                btnSubmit.Enabled = false;
+            }
+            
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            int a = 0;
+            using(SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["TeamsDatabaseConnString"].ConnectionString))
+            {
+                conn.Open();
+                using (SqlCommand cmd = new SqlCommand("INSERT INTO [User] (Username, Password, Name, IC, Phone, Image, isAdmin) VALUES (@Uname, @pwd, @name, @ic, @phone, @image, @a)", conn))
+                {
+                cmd.Parameters.AddWithValue("@Uname", txtUname.Text);
+                cmd.Parameters.AddWithValue("@pwd", txtPwd.Text);
+                cmd.Parameters.AddWithValue("@name", txtName.Text);
+                cmd.Parameters.AddWithValue("@ic", txtIc.Text);
+                cmd.Parameters.AddWithValue("@phone", txtPhone.Text);
+                cmd.Parameters.AddWithValue("@image", txtImage.Text);
+                cmd.Parameters.AddWithValue("@a", a);
+                int rowsAffected = cmd.ExecuteNonQuery();
+                    //Response.Write("<script>alert('Register successfully!');</script>");
+                    if (rowsAffected > 0)
+                    {
+                        // Successful registration
+                        Response.Write("<script>alert('Register successfully!');</script>");
+                    }
+                    else
+                    {
+                        // Registration failed
+                        Response.Write("<script>alert('Registration failed!');</script>");
+                    }
+                }
+                    
+                
+                conn.Close();
+            }
+            
+        }
+
+        protected void btnCancel_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("login.aspx");
+        }
+
+        protected void cbxTerm_CheckedChanged(object sender, EventArgs e)
+        {
+            
+            if(cbxTerm.Checked)
+            {
+                btnSubmit.Enabled = true;
+            }
+            else
+            {
+                btnSubmit.Enabled = false;
+            }
+        }
+    }
+}
