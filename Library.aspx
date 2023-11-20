8f2eced3ad0b16b405b5010bb332f3b05b1009f2
chiayinnn <150212453+chiayinnn@users.noreply.github.com>
2023-11-21 07:06:21 +0800

    Add files via upload

---
+++ Library.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" MasterPageFile="~/Teams.Master" AutoEventWireup="true" CodeBehind="Library.aspx.cs" Inherits="GroupASS.Library" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+        <div class=" col-lg-8" style="margin-top:30px">
+            <h1>Library</h1>
+            <asp:Panel ID="pnlLibrary" runat="server" GroupingText="Recent Games">
+            <asp:DataList ID="dltLibrary" runat="server" RepeatDirection="Horizontal">
+                <ItemTemplate>
+                    <div style="width:350px; text-align:center">
+                    <asp:Image ID="imgLibrary" runat="server" ImageUrl='<%#Eval("Image","~/Images/{0}") %>' width="200px" Height="120px" />
+                        <br />
+                    <asp:Label ID="lblName" runat="server" Text='<%#Eval("Name") %>'></asp:Label>
+                        <br />
+                    <asp:Label ID="lblCategory" runat="server" Text='<%# "Category: " + Eval("CategoryName") %>'></asp:Label>
+                        <br />
+                    <asp:LinkButton ID="lbtnMore" runat="server" Text="Click Here for More Information" />
+                </ItemTemplate>
+            </asp:DataList>
+            </asp:Panel>
+        </div>
+</asp:Content>
---
+++ Library.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace GroupASS
+{
+    public partial class Library : System.Web.UI.Page
+    {
+        SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["TeamsDatabase"].ConnectionString);
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!this.IsPostBack)
+            {
+                this.BindGrid();
+            }
+        }
+
+        private void BindGrid()
+        {
+            string constr = ConfigurationManager.ConnectionStrings["TeamsDatabase"].ConnectionString;
+
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                using (SqlCommand cmd = new SqlCommand("SELECT Payment.Username,Payment.GameID,Games.GameID,Games.Name,Games.CategoryID,Games.Image,Games.Description,Categories.CategoryID,Categories.CategoryName FROM Payment INNER JOIN Games ON Payment.GameID = Games.GameID INNER JOIN Categories ON Games.CategoryID = Categories.CategoryID", conn))
+                {
+                    using (SqlDataAdapter sda = new SqlDataAdapter())
+                    {
+                        cmd.Connection = con;
+                        sda.SelectCommand = cmd;
+
+                        using (DataTable dt = new DataTable())
+                        {
+                            sda.Fill(dt);
+                            dltLibrary.DataSource = dt;
+                            dltLibrary.DataBind();
+                        }
+
+                    }
+                }
+            }
+        }
+            
+    }
+}
