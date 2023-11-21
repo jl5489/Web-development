b7b5c2d60c3ffaf5e83a7e6b29de7a68e2675f3a
jl5489 <105220617+jl5489@users.noreply.github.com>
2023-11-21 10:12:18 +0800

    Add files via upload

---
+++ EditGames.aspx
@@ -0,0 +1,169 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Teams.Master" AutoEventWireup="true" CodeFile="EditGames.aspx.cs" Inherits="groupassignment.EditGames" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+           <aside><h2>Edit the games</h2></aside>
+   <article>
+
+       <div>
+           Game ID:
+           <asp:TextBox ID="txtSearch" runat="server"></asp:TextBox>
+           &nbsp;&nbsp;
+           <asp:Button ID="btnSearch" runat="server" Text="Search" OnClick="btnSearch_Click"/>
+           <br />
+           <asp:Label ID="lblResult" runat="server" ></asp:Label>
+           <br /><br />
+           <asp:Label ID="lblStatus" runat="server"></asp:Label>
+           <asp:Label ID="lbHeader" runat="server" Text="Add New Record" Font-Size="Larger"></asp:Label>
+           <asp:Table ID="tabEditing" runat="server">
+               <asp:TableRow>
+                   <asp:TableCell>
+                       Game ID:
+                   </asp:TableCell>
+                   <asp:TableCell>
+                       <asp:TextBox ID="txtGameID" runat="server" />
+                   </asp:TableCell>
+               </asp:TableRow>
+               <asp:TableRow>
+                   <asp:TableCell>
+                       Name:
+                   </asp:TableCell>
+                   <asp:TableCell>
+                       <asp:TextBox ID="txtName" runat="server"></asp:TextBox>
+                   </asp:TableCell>
+               </asp:TableRow>
+               <asp:TableRow>
+                   <asp:TableCell>
+                       CategoryID:
+                   </asp:TableCell>
+                   <asp:TableCell>
+                       <asp:DropDownList ID="ddlCate" runat="server">
+                           <asp:ListItem>CategoryID List</asp:ListItem>
+                           <asp:ListItem Value="c1">c1 Action</asp:ListItem>
+                           <asp:ListItem Value="c2">c2 Horror</asp:ListItem>
+                           <asp:ListItem Value="c3">c3 Strategy</asp:ListItem>
+                           <asp:ListItem Value="c4">c4 Sport & Racing</asp:ListItem>
+                           <asp:ListItem Value="c5">c5 Adventure</asp:ListItem>
+                           <asp:ListItem Value="c6">c6 Simulation</asp:ListItem>
+                       </asp:DropDownList>                   
+                   </asp:TableCell>
+               </asp:TableRow>
+               <asp:TableRow>
+                   <asp:TableCell>
+                       Price:
+                   </asp:TableCell>
+                   <asp:TableCell>
+                       <asp:TextBox ID="txtPrice" runat="server"></asp:TextBox>
+                   </asp:TableCell>
+               </asp:TableRow>
+               <asp:TableRow>
+                   <asp:TableCell>
+                       Description:
+                   </asp:TableCell>
+                   <asp:TableCell>
+                        <asp:TextBox ID="txtDesc" runat="server"  TextMode="MultiLine" MaxLength="50"/>
+                   </asp:TableCell>
+               </asp:TableRow>
+               <asp:TableRow>
+                            <asp:TableCell>
+                                Photo:
+                            </asp:TableCell>
+                            <asp:TableCell>
+                                <asp:Image ID="imgProdPhoto" runat="server" Height="100" Width="80"/>
+                                <asp:LinkButton ID="lbtnProdPhoto" runat="server" OnClick="lbtnProdPhoto_Click" Text="Change photo?"></asp:LinkButton>
+                                <asp:FileUpload ID="fiuImage" runat="server" />
+                                <br />
+                                <asp:LinkButton ID="lbtnCancel" runat="server" OnClick="lbtnCancel_Click">Cancel Upload</asp:LinkButton>
+                                <br />
+                                <asp:Label ID="lblUploadStatus" runat="server" ForeColor="Red" />
+                                <asp:Label ID="lblFileName" runat="server" Visible="false"></asp:Label>
+                            </asp:TableCell>
+               </asp:TableRow>
+               <asp:TableRow>
+                            <asp:TableCell>
+                                Advertiment:
+                            </asp:TableCell>
+                            <asp:TableCell>
+                                <asp:Image ID="imgAD" runat="server" Height="100" Width="80"/>
+                                <asp:LinkButton ID="lbtnAD" runat="server" OnClick="lbtnProdPhoto_Click" Text="Change photo?"></asp:LinkButton>
+                                <asp:FileUpload ID="fiuAD" runat="server" />
+                                <br />
+                                <asp:LinkButton ID="lbtnCancel1" runat="server" OnClick="lbtnCancel_Click">Cancel Upload</asp:LinkButton>
+                                <br />
+                                <asp:Label ID="lblUploadStatus1" runat="server" ForeColor="Red" />
+                                <asp:Label ID="lblFilename1" runat="server" Visible="false"></asp:Label>
+                            </asp:TableCell>
+               </asp:TableRow>
+               <asp:TableRow>
+                   <asp:TableCell>
+                       &nbsp;
+                   </asp:TableCell>
+                   <asp:TableCell>
+                       <asp:Button ID="btnSave" runat="server" Text="Save" OnClick="btnSave_Click"/>
+                       <asp:Button ID="btnUpdate" runat="server" Text="Update" OnClick="btnUpdate_Click"/>
+                       <asp:Button ID="btnDelete" runat="server" Text="Delete" OnClick="btnDelete_Click"/>
+                       <asp:Button ID="btnClear" runat="server" Text="Clear" OnClick="btnClear_Click" />
+                   </asp:TableCell>
+               </asp:TableRow>
+           </asp:Table>
+           <br />
+           <asp:Table ID="tabResult" runat="server">
+               <asp:TableRow>
+                   <asp:TableCell>
+                       Game ID:
+                   </asp:TableCell>
+                   <asp:TableCell>
+                       <asp:Label ID="lblGameID" runat="server"></asp:Label>
+                   </asp:TableCell>
+               </asp:TableRow>
+               <asp:TableRow>
+                   <asp:TableCell>
+                       Name:
+                   </asp:TableCell>
+                   <asp:TableCell>
+                       <asp:Label ID="lblName" runat="server"></asp:Label>
+                   </asp:TableCell>
+               </asp:TableRow>
+               <asp:TableRow>
+                   <asp:TableCell>
+                       CategoryID:
+                   </asp:TableCell>
+                   <asp:TableCell>
+                       <asp:Label ID="lblCate" runat="server"></asp:Label>
+                   </asp:TableCell>
+               </asp:TableRow>
+               <asp:TableRow>
+                   <asp:TableCell>Price</asp:TableCell>
+                   <asp:TableCell>
+                       <asp:Label ID="lblPrice" runat="server" Text="Label"></asp:Label>
+                   </asp:TableCell>
+               </asp:TableRow>
+               <asp:TableRow>
+                   <asp:TableCell>
+                       Description:
+                   </asp:TableCell>
+                   <asp:TableCell>
+                       <asp:Label ID="lblDesc" runat="server" Text="Label"></asp:Label>
+                   </asp:TableCell>
+               </asp:TableRow>
+                <asp:TableRow>
+                    <asp:TableCell>Photo:</asp:TableCell>
+                    <asp:TableCell>
+                        <asp:Image ID="imgGame" runat="server" />
+                    </asp:TableCell>
+                </asp:TableRow>
+                <asp:TableRow>
+                    <asp:TableCell>Advertisment:</asp:TableCell>
+                    <asp:TableCell>
+                        <asp:Image ID="imgDisplay" runat="server" />              
+                    </asp:TableCell>
+                </asp:TableRow>
+               <asp:TableRow>
+                   <asp:TableCell>
+                       <asp:Button ID="btnEdit" runat="server" Text="Edit" OnClick="btnEdit_Click"/>
+                   </asp:TableCell>
+               </asp:TableRow>
+           </asp:Table>
+       </div>
+       </article>
+</asp:Content>
---
+++ EditGames.aspx.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace groupassignment
+{
+    public partial class EditGames : System.Web.UI.Page
+    {
+        SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["TeamsConnString"].ConnectionString);
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                tabResult.Visible = false;
+                imgProdPhoto.Visible = false;
+                imgAD.Visible = false;
+                lbtnCancel.Visible = false;
+                lbtnCancel1.Visible = false;
+                lbtnProdPhoto.Visible = false;
+                lbtnAD.Visible = false;
+                btnUpdate.Visible = false;
+                btnDelete.Visible = false;
+            }
+        }
+        public void Clear()
+        {
+            txtGameID.Text = txtName.Text = txtPrice.Text = txtSearch.Text = txtDesc.Text = "";
+            lblResult.Text = lblUploadStatus.Text = lblUploadStatus1.Text = "";
+            imgProdPhoto.ImageUrl = imgAD.ImageUrl = "";
+            imgProdPhoto.Visible = false;
+            imgAD.Visible = false;
+            ddlCate.SelectedIndex = 0;
+            txtGameID.Enabled = true;
+            btnUpdate.Visible = false;
+            btnDelete.Visible = false;
+            btnSave.Visible = true;
+            lbtnProdPhoto.Visible = false;
+            lbtnAD.Visible = false;
+            fiuImage.Visible = true;
+            fiuAD.Visible = true;
+            lbtnCancel.Visible = false;
+            lbtnCancel1.Visible = false;
+            lbHeader.Text = "Add new Record";
+            tabEditing.Visible = true;
+
+        }
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("SELECT * FROM[Games] WHERE GameID=@GameId", conn);
+            cmd.Parameters.AddWithValue("GameId", txtSearch.Text);
+
+            SqlDataReader dr = cmd.ExecuteReader();
+            bool recordfound = dr.Read();
+
+            if (recordfound)
+            {
+                tabResult.Visible = true;
+                tabEditing.Visible = false;
+                lblResult.Text = "Record found!";
+                lblResult.ForeColor = System.Drawing.Color.Green;
+                lbHeader.Text = "Record Details";
+                lblGameID.Text = dr["GameID"].ToString();
+                lblName.Text = dr["Name"].ToString();
+                lblPrice.Text = string.Format("{0:c}", dr["Price"]);
+                imgGame.ImageUrl = "~/TeamsImages/" + dr["Image"].ToString();
+                lblDesc.Text = dr["Description"].ToString();
+                imgDisplay.ImageUrl = "~/TeamsImages/" + dr["Ad"].ToString();
+            }
+            else
+            {
+                tabResult.Visible = false;
+                lblResult.Text = "No record found...";
+                lblResult.ForeColor = System.Drawing.Color.White;
+                lblResult.BackColor = System.Drawing.Color.Red;
+                tabResult.Visible = false;
+            }
+            conn.Close();
+        }
+
+        public void uploadFile()
+        {
+            string filename;
+            string filename1;
+            if (fiuImage.HasFile)
+            {
+                try
+                {
+                    filename = System.IO.Path.GetFileName(fiuImage.PostedFile.FileName);
+                    fiuImage.PostedFile.SaveAs(Server.MapPath("~/TeamsImages/" + fiuImage.FileName));
+                    lblUploadStatus.Text = filename + "uploaded.";
+                    lblFileName.Text = filename;
+                }
+                catch
+                {
+                    lblUploadStatus.Text = "Unable to save file";
+                }
+            }
+            else
+            {
+                lblUploadStatus.Text = "No file selected";
+            }
+            if (fiuAD.HasFile)
+            {
+                try
+                {
+                    filename1 = System.IO.Path.GetFileName(fiuAD.PostedFile.FileName);
+                    fiuImage.PostedFile.SaveAs(Server.MapPath("~/TeamsImages/" + fiuAD.FileName));
+                    lblUploadStatus1.Text = filename1 + "uploaded.";
+                    lblFilename1.Text = filename1;
+                }
+                catch
+                {
+                    lblUploadStatus1.Text = "Unable to save file";
+                }
+            }
+            else
+            {
+                lblUploadStatus1.Text = "No file selected";
+
+            }
+        }
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            uploadFile();
+
+            try
+            {
+                if (conn.State == System.Data.ConnectionState.Closed)
+                {
+                    conn.Open();
+                    string insertQuery = "INSERT INTO Games(GameID,Name,Description,"+"CategoryID,Image,Price,Ad)" + "values(@gaID,@gaName,@gaDes,@catID,@filename,@price,@Adver)";
+                    SqlCommand com = new SqlCommand(insertQuery, conn);
+                    com.Parameters.AddWithValue("gaID", txtGameID.Text);
+                    com.Parameters.AddWithValue("gaName", txtName.Text);
+                    com.Parameters.AddWithValue("gaDes", txtDesc.Text);
+                    com.Parameters.AddWithValue("catID", ddlCate.SelectedValue);
+                    com.Parameters.AddWithValue("@filename", lblFileName.Text);
+                    com.Parameters.AddWithValue("@price", txtPrice.Text);
+                    com.Parameters.AddWithValue("@Adver", lblFilename1.Text);
+
+                    com.ExecuteNonQuery();
+                    Response.Write("<script>alert('New Product added successfully!');</script>");
+                    conn.Close();
+                    Clear();
+                }
+            }
+            catch (Exception ex)
+            {
+                lblResult.Text = "Error:" + ex.ToString();
+                lblResult.ForeColor = System.Drawing.Color.Red;
+            }
+        }
+
+        protected void btnEdit_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("SELECT * FROM[Games] WHERE GameID=@GameId", conn);
+            cmd.Parameters.AddWithValue("GameId", txtSearch.Text);
+
+            SqlDataReader dr = cmd.ExecuteReader();
+            bool recordfound = dr.Read();
+
+            if (recordfound)
+            {
+                lbHeader.Text = "Update Record";
+                txtGameID.Text = dr["GameID"].ToString();
+                txtGameID.Enabled = false;
+                txtName.Text = dr["Name"].ToString();
+                txtPrice.Text = string.Format("{0:c}", dr["Price"]);
+                lblFileName.Text = dr["Image"].ToString();
+                lblFilename1.Text = dr["Ad"].ToString();
+                ddlCate.SelectedValue = dr["CategoryID"].ToString();
+                imgGame.ImageUrl = "~/TeamsImages/" + dr["Image"].ToString();
+                imgDisplay.ImageUrl = "~/TeamsImages/" + dr["Ad"].ToString();
+                txtDesc.Text = dr["Description"].ToString();
+            }
+
+            tabResult.Visible = false;
+            tabEditing.Visible = true;
+            btnSave.Visible = false;
+            btnUpdate.Visible = true;
+            btnDelete.Visible = true;
+            btnClear.Visible = true;
+            imgProdPhoto.Visible = true;
+            imgAD.Visible = true;
+            lbtnAD.Visible = true;
+            lbtnProdPhoto.Visible = true;
+            fiuImage.Visible = false;
+            fiuAD.Visible = false;
+            conn.Close();
+
+        }
+    
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            uploadFile();
+
+            if (conn.State == System.Data.ConnectionState.Closed)
+            {
+                conn.Open();
+                string updateQuery = "UPDATE Games SET Name=@name,Description=@Des," + "CategoryID=@catID,Image=@filename,Price=@price,Ad=@adver WHERE GameID=@Id";
+                SqlCommand com = new SqlCommand(updateQuery, conn);
+                {
+                    //@shortName, @longName, @Id
+                    com.Parameters.AddWithValue("@Id", txtGameID.Text);
+                    com.Parameters.AddWithValue("@name", txtName.Text);
+                    com.Parameters.AddWithValue("@Des", txtDesc.Text);
+                    com.Parameters.AddWithValue("@catID", ddlCate.SelectedValue);
+                    com.Parameters.AddWithValue("@filename", lblFileName.Text);
+                    com.Parameters.AddWithValue("@price", txtPrice.Text);
+                    com.Parameters.AddWithValue("@adver", lblFilename1.Text);
+
+                    com.ExecuteNonQuery();
+                }
+                Response.Write("<script>alert('Record Updated Successfully!');</script>");
+                com.Dispose();
+                conn.Close();
+                Clear();
+            }
+        }
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (conn.State == System.Data.ConnectionState.Closed)
+            {
+                try
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = new SqlCommand("DELETE FROM Games WHERE GameID=@Id", conn))
+                    {
+                        cmd.Parameters.AddWithValue("Id", txtGameID.Text);
+                        int rows = cmd.ExecuteNonQuery();
+
+                        Response.Write("<script>alert('Record Deleted Successfully...')</script>");
+                        conn.Close();
+                        Clear();
+                    }
+                }
+
+                catch (SqlException ex)
+                {
+                    lblResult.Text = "Error found: " + ex.Message;
+                    lblResult.ForeColor = System.Drawing.Color.Red;
+                }
+            }
+        }
+
+        protected void btnClear_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+
+        protected void lbtnProdPhoto_Click(object sender, EventArgs e)
+        {
+            fiuImage.Visible = true;
+            fiuAD.Visible = true;
+            imgProdPhoto.Visible = true;
+            imgAD.Visible = true;
+            lbtnProdPhoto.Visible = false;
+            lbtnAD.Visible = false;
+            lbtnCancel.Visible = true;
+            lbtnCancel1.Visible = true;
+        }
+
+        protected void lbtnCancel_Click(object sender, EventArgs e)
+        {
+            fiuImage.Visible = false;
+            fiuAD.Visible = false;
+            imgAD.Visible = true;
+            imgProdPhoto.Visible = true;
+            lbtnProdPhoto.Visible = true;
+            lbtnAD.Visible = true; 
+            lbtnCancel1.Visible = false;
+            lbtnCancel.Visible = false;
+
+            Clear();
+        }
+    }
+}
