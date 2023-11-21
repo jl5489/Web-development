d8f97e516a782ba7f8842e31af8231cc795a2d40
jl5489 <105220617+jl5489@users.noreply.github.com>
2023-11-21 11:43:51 +0800

    Update Library.aspx.cs

--- Library.aspx.cs
+++ Library.aspx.cs
@@ -46,6 +46,82 @@ namespace GroupASS
                 }
             }
         }
-            
+
+        protected void imbAdverList1_Click(object sender, ImageClickEventArgs e)
+        {
+            Response.Redirect("~/Gamedetail.aspx?IDGa=g0001" );
+
+        }
+
+        protected void imbAdverList2_Click(object sender, ImageClickEventArgs e)
+        {
+            Response.Redirect("~/Gamedetail.aspx?IDGa=g0001");
+        }
+
+        protected void imbAdverList3_Click(object sender, ImageClickEventArgs e)
+        {
+            Response.Redirect("~/Gamedetail.aspx?IDGa=g0001");
+        }
+
+        protected void imbAdverList4_Click(object sender, ImageClickEventArgs e)
+        {
+            Response.Redirect("~/Gamedetail.aspx?IDGa=g0002");
+        }
+
+        protected void imbAdverList5_Click(object sender, ImageClickEventArgs e)
+        {
+            Response.Redirect("~/Gamedetail.aspx?IDGa=g0003");
+        }
+
+        protected void imbCatelistAdv_Click(object sender, ImageClickEventArgs e)
+        {
+            Response.Redirect("~/Category.aspx?IDCAT=c5");
+        }
+
+        protected void imbCatelistHorror_Click(object sender, ImageClickEventArgs e)
+        {
+            Response.Redirect("~/Category.aspx?IDCAT=c2");
+        }
+
+        protected void imbCatelistSimu_Click(object sender, ImageClickEventArgs e)
+        {
+            Response.Redirect("~/Category.aspx?IDCAT=c6");
+
+        }
+
+        protected void imbCatelistSport_Click(object sender, ImageClickEventArgs e)
+        {
+            Response.Redirect("~/Category.aspx?IDCAT=c4");
+
+        }
+
+        protected void imbCatelistStra_Click(object sender, ImageClickEventArgs e)
+        {
+            Response.Redirect("~/Category.aspx?IDCAT=c3");
+
+        }
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("SELECT * FROM[Games] WHERE Name=@ganame", conn);
+            cmd.Parameters.AddWithValue("ganame", txtSearch.Text);
+            SqlDataReader dr = cmd.ExecuteReader();
+            bool recordfound = dr.Read();
+
+            if (recordfound)
+            {
+                String GaID = dr["GameID"].ToString();
+                Response.Redirect("~/Gamedetail.aspx?IDGa=GaID");
+            }
+            else
+            {
+
+                lblResult.Text = "No record found...";
+                lblResult.ForeColor = System.Drawing.Color.Red;
+
+            }
+            conn.Close();
+        }
     }
 }
+    
